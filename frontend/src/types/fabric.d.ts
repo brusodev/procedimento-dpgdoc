c2a9a3e0732ca17b986e4a6c3fd6901685c4c41b
Bruno Soares <brunosoaresvargas@gmail.com>
2025-11-26 17:31:06 -0300

    Fix: adicionar definições completas de tipos do fabric.js

--- frontend/src/types/fabric.d.ts
+++ frontend/src/types/fabric.d.ts
@@ -1,3 +1,61 @@
 declare module 'fabric' {
-  export const fabric: any
+  export namespace fabric {
+    class Canvas {
+      constructor(element: HTMLCanvasElement | null, options?: any): Canvas
+      add(...objects: any[]): Canvas
+      remove(...objects: any[]): Canvas
+      getObjects(): any[]
+      getPointer(e: any): any
+      on(eventName: string, handler: (e: any) => void): void
+      renderAll(): void
+      setWidth(width: number): void
+      setHeight(height: number): void
+      dispose(): void
+      [key: string]: any
+    }
+
+    class Object {
+      left?: number
+      top?: number
+      width?: number
+      height?: number
+      selectable?: boolean
+      [key: string]: any
+    }
+
+    interface IEvent {
+      e: any
+      selected?: any[]
+      [key: string]: any
+    }
+
+    namespace Image {
+      function fromURL(url: string, callback: (img: any) => void): void
+    }
+
+    class Rect {
+      constructor(options?: any): Rect
+      [key: string]: any
+    }
+
+    class Line {
+      constructor(points: number[], options?: any): Line
+      [key: string]: any
+    }
+
+    class Circle {
+      constructor(options?: any): Circle
+      [key: string]: any
+    }
+
+    class Triangle {
+      constructor(options?: any): Triangle
+      [key: string]: any
+    }
+
+    class Textbox {
+      constructor(text: string, options?: any): Textbox
+      [key: string]: any
+    }
+  }
 }
